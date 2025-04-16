421c2ac5b536a3d378552d015e83a11a2c9bc4ae
Pratheeksha Poonja <Pratheekshapoonja@gmail.com>
2025-04-16 18:49:49 +0530

    Final UI changes

--- src/ReactQnA.jsx
+++ src/ReactQnA.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { Questions } from "./questions";
-import { Minus, Plus } from "react-feather";
+import { Maximize, Minimize, Minus, Plus } from "react-feather";
+import Zoom from "./assets/zoom.jpg";
 import { CategoryList } from "./utils/CategoryList";
+import Tippy from "@tippyjs/react";
+import { ZOOM_LEVEL } from "./constants";
 
 export function ReactQnA() {
   const [displayAns, setDisplayAns] = useState([]);
   const [questions, setQuestions] = useState(Questions);
   const [categorySelected, setCategorySelected] = useState("All");
+  const [zoomNumber, setZoomNumber] = useState(ZOOM_LEVEL[1]);
 
   const expandDropdown = (index) => {
     if (displayAns.includes(index)) {
@@ -15,6 +19,21 @@ export function ReactQnA() {
     } else {
       setDisplayAns((prev) => [...prev, index]);
     }
+  };
+
+  const collapseAll = () => {
+    setDisplayAns([]);
+  };
+
+  const expandAll = () => {
+    const allIndexes = Questions.map((_, index) => index);
+    setDisplayAns(allIndexes);
+  };
+
+  const zoomGrid = () => {
+    const currentIndex = ZOOM_LEVEL.indexOf(zoomNumber);
+    const nextIndex = (currentIndex + 1) % ZOOM_LEVEL.length;
+    setZoomNumber(ZOOM_LEVEL[nextIndex]);
   };
 
   useEffect(() => {
@@ -31,7 +50,30 @@ export function ReactQnA() {
   return (
     <>
       <CategoryList onClick={setCategorySelected} />
-      <main className="quiz-section">
+      <div className="full-flex-inline">
+        <Tippy content="Expand All">
+          <Maximize onClick={expandAll} className="react-qna-icons" />
+        </Tippy>
+        <Tippy content="Collapse All">
+          <Minimize onClick={collapseAll} className="react-qna-icons" />
+        </Tippy>
+        <Tippy content="Zoom-in/Zoom-out">
+          <img
+            src={Zoom}
+            className="react-qna-icons"
+            alt="zoom"
+            width="27"
+            height="27"
+            onClick={zoomGrid}
+          />
+        </Tippy>
+      </div>
+      <main
+        className="quiz-section"
+        style={{
+          "grid-template-columns": `repeat(auto-fit, minmax(${zoomNumber}%, 1fr))`,
+        }}
+      >
         {questions?.map((item, index) => (
           <section className="react-qna-section" key={index}>
             <div key={index} className="react-qna-sub-sec">
--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,14 @@
-export const TITLE='Welcome to Think & React'
+export const TITLE = "Welcome to Think & React";
 
-export const QUIZ_TYPE={
-    QnA:'Question & Answers',
-    Quiz:'React Quiz',
-    QuickQuiz:'Quick Quiz'
-}
-export const QUIZ_TOOLTIP={
-    QnA:'Browse or interact with curated questions and answers to reinforce your understanding of key concepts in a simple, conversational format.',
-    Quiz:'Take fun and challenging quizzes with instant scoring. Great for testing your memory, sharpening your skills, or just learning something new!',
-    QuickQuiz:'Put your knowledge to the test - no hints, no help. Finish strong and reveal your final score when it’s done.'
-}
+export const QUIZ_TYPE = {
+  QnA: "Question & Answers",
+  Quiz: "Quiz",
+  QuickQuiz: "Quick Quiz",
+};
+export const QUIZ_TOOLTIP = {
+  QnA: "Browse or interact with curated questions and answers to reinforce your understanding of key concepts in a simple, conversational format.",
+  Quiz: "Take fun and challenging quizzes with instant scoring. Great for testing your memory, sharpening your skills, or just learning something new!",
+  QuickQuiz:
+    "Put your knowledge to the test - no hints, no help. Finish strong and reveal your final score when it’s done.",
+};
+export const ZOOM_LEVEL = [31, 49, 100];
